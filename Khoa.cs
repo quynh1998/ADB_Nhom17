7d392861363a83ed8cf786f55ebc9b935accf75a
quynh1998 <37840050+quynh1998@users.noreply.github.com>
2018-11-07 01:01:29 +0700

    Add files via upload

---
+++ Khoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2016
+{
+    [Serializable]
+    public class Khoa
+    {
+        public string MaKhoa { get; set; }
+        public string TenKhoa { get; set; }
+        public string DiaChi { get; set; }
+        public string DienThoai { get; set; }
+
+        public override string ToString()
+        {
+            return TenKhoa;
+        }
+    }
+}
